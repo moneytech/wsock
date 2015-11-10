f780f724db9c924e51c173d708593234522e6b1e
Martin Sustrik <sustrik@250bpm.com>
2015-11-10 04:13:31 +0100

    Initial handshake semi-functional
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- wsock.c
+++ wsock.c
@@ -32,6 +32,7 @@
 #include <sys/time.h>
 
 #include "base64.h"
+#include "sha1.h"
 #include "wsock.h"
 
 struct wsock {
@@ -92,101 +93,127 @@ wsock wsocklisten(ipaddr addr, int backlog) {
 }
 
 wsock wsockaccept(wsock s, int64_t deadline) {
+    int err = 0;
     struct wsock *as = (struct wsock*)malloc(sizeof(struct wsock));
+    if(!as) {err = ENOMEM; goto err0;}
     as->u = tcpaccept(s->u, deadline);
-    if(errno != 0) {
-        free(as);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err1;}
+
+    // Parse request.
     char buf[256];
     size_t sz = wsockgetline(as, buf, sizeof(buf), deadline);
-    if(sz < 0) {
-        tcpclose(as->u);
-        free(as);
-        return NULL;
-    }
+    if(sz < 0) {err = errno; goto err2;}
     char *lend = buf + sz;
     char *wstart = buf;
     char *wend = (char*)memchr(buf, ' ', lend - wstart);
     if(!wend || wend - wstart != 3 || memcmp(wstart, "GET", 3) != 0) {
-        tcpclose(as->u);
-        free(as);
-        errno = EPROTO;
-        return NULL;
-    }
+        err = EPROTO; goto err2;}
     wstart = wend + 1;
     wend = (char*)memchr(wstart, ' ', lend - wstart);
-    if(!wend) {
-        tcpclose(as->u);
-        free(as);
-        errno = EPROTO;
-        return NULL;
-    }
-    // TODO: Store the URL.
+    if(!wend) {err = EPROTO; goto err2;}
+    /* TODO: Store the URL. */
     wstart = wend + 1;
     wend = (char*)memchr(wstart, ' ', lend - wstart);
     if(wend || lend - wstart != 8 || memcmp(wstart, "HTTP/1.1", 8) != 0) {
-        tcpclose(as->u);
-        free(as);
-        errno = EPROTO;
-        return NULL;
-    }
-    // Parse individual fields.
+        err = EPROTO; goto err2;}
+    int hasupgrade = 0;
+    int hasconnection = 0;
+    int haskey = 0;
+    struct wsock_sha1 sha1;
     while(1) {
         sz = wsockgetline(as, buf, sizeof(buf), deadline);
-        if(sz < 0) {
-            tcpclose(as->u);
-            free(as);
-            return NULL;
-        }
+        if(sz < 0) {err = errno; goto err2;}
         if(sz == 0)
             break;
-        // TODO
+        lend = buf + sz;
+        char *nstart = buf;
+        char *nend = (char*)memchr(buf, ' ', lend - nstart);
+        if(!nend || nend - nstart < 1 || nend[-1] != ':') {
+            err = EPROTO; goto err2;}
+        size_t nsz = nend - nstart - 1;
+        char *vstart = nend + 1;
+        char *vend = (char*)memchr(vstart, ' ', lend - vstart);
+        if(vend) {err = EPROTO; goto err2;}
+        size_t vsz = lend - vstart;
+        if(nsz == 7 && memcmp(nstart, "Upgrade", 7) == 0) {
+            if(hasupgrade || vsz != 9 || memcmp(vstart, "websocket", 9) != 0) {
+                err = EPROTO; goto err2;}
+            hasupgrade = 1;
+            continue;
+        }
+        if(nsz == 10 && memcmp(nstart, "Connection", 10) == 0) {
+            if(hasconnection || vsz != 7 || memcmp(vstart, "Upgrade", 7) != 0) {
+                err = EPROTO; goto err2;}
+            hasconnection = 1;
+            continue;
+        }
+        if(nsz == 17 && memcmp(nstart, "Sec-WebSocket-Key", 17) == 0) {
+            if(haskey) {err = EPROTO; goto err2;}
+            wsock_sha1_init(&sha1);
+            int i;
+            for(i = 0; i != vsz; ++i)
+                wsock_sha1_hashbyte(&sha1, vstart[i]);
+            const char *uuid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+            for(i = 0; i != 36; ++i)
+                wsock_sha1_hashbyte(&sha1, uuid[i]);
+            haskey = 1;
+            continue;
+        }
     }
+    if(!hasupgrade || !hasconnection || !haskey) {err = EPROTO; goto err2;}
 
-    assert(0);
+    /* Send reply. */
+    const char *lit1 =
+        "HTTP/1.1 101 Switching Protocols\r\n"
+        "Upgrade: websocket\r\n"
+        "Connection: Upgrade\r\n"
+        "Sec-WebSocket-Accept: ";
+    tcpsend(as->u, lit1, strlen(lit1), deadline);
+    if(errno != 0) {err = errno; goto err2;}
+    char key[32];
+    wsock_sha1_result(&sha1);
+    sz = wsock_base64_encode(wsock_sha1_result(&sha1), 20, key, sizeof(key));
+    assert(sz > 0);
+    tcpsend(as->u, key, sz, deadline);
+    if(errno != 0) {err = errno; goto err2;}
+    tcpsend(as->u, "\r\n\r\n", 4, deadline);
+    if(errno != 0) {err = errno; goto err2;}
+    tcpflush(as->u, deadline);
+    if(errno != 0) {err = errno; goto err2;}
+
+    return as;
+
+err2:
+    tcpclose(as->u);
+err1:
+    free(as);
+err0:
+    errno = err;
+    return NULL;
 }
 
 wsock wsockconnect(ipaddr addr, const char *url, int64_t deadline) {
+    int err = 0;
     struct wsock *s = (struct wsock*)malloc(sizeof(struct wsock));
-    if(!s) {
-        errno = ENOMEM;
-        return NULL;
-    }
+    if(!s) {err = ENOMEM; goto err0;}
     s->u = tcpconnect(addr, deadline);
-    if(errno != 0) {
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err1;}
+
+    /* Send request. */
     tcpsend(s->u, "GET ", 4, deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
     tcpsend(s->u, url, strlen(url), deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
     const char *lit1 =
         " HTTP/1.1\r\n"
         "Upgrade: websocket\r\n"
         "Connection: Upgrade\r\n"
         "Sec-WebSocket-Key: ";
     tcpsend(s->u, lit1, strlen(lit1), deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
+    /* TODO: Set the seed once only? */
     struct timeval tv;
-    if(gettimeofday(&tv, NULL) == -1) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(gettimeofday(&tv, NULL) == -1) {err = errno; goto err2;}
     srandom((int)tv.tv_usec);
     uint8_t nonce[16];
     int i;
@@ -196,26 +223,72 @@ wsock wsockconnect(ipaddr addr, const char *url, int64_t deadline) {
     int swsk_len = wsock_base64_encode(nonce, 16, swsk, sizeof(swsk));
     assert(swsk_len > 0);
     tcpsend(s->u, swsk, swsk_len, deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
     tcpsend(s->u, "\r\n\r\n", 4, deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
     tcpflush(s->u, deadline);
-    if(errno != 0) {
-        tcpclose(s->u);
-        free(s);
-        return NULL;
-    }
+    if(errno != 0) {err = errno; goto err2;}
 
-    msleep(now() + 1000);
-    assert(0);
+    /* Parse reply. */
+    char buf[256];
+    size_t sz = wsockgetline(s, buf, sizeof(buf), deadline);
+    if(sz < 0) {err = errno; goto err2;}
+    char *lend = buf + sz;
+    char *wstart = buf;
+    char *wend = (char*)memchr(buf, ' ', lend - wstart);
+    if(!wend || wend - wstart != 8 || memcmp(wstart, "HTTP/1.1", 8) != 0) {
+        err = EPROTO; goto err2;}
+    wstart = wend + 1;
+    wend = (char*)memchr(wstart, ' ', lend - wstart);
+    if(!wend || wend - wstart != 3 || memcmp(wstart, "101", 3) != 0) {
+        err = EPROTO; goto err2;}
+    int hasupgrade = 0;
+    int hasconnection = 0;
+    int haskey = 0;
+    while(1) {
+        sz = wsockgetline(s, buf, sizeof(buf), deadline);
+        if(sz < 0) {err = errno; goto err2;}
+        if(sz == 0)
+            break;
+        lend = buf + sz;
+        char *nstart = buf;
+        char *nend = (char*)memchr(buf, ' ', lend - nstart);
+        if(!nend || nend - nstart < 1 || nend[-1] != ':') {
+            err = EPROTO; goto err2;}
+        size_t nsz = nend - nstart - 1;
+        char *vstart = nend + 1;
+        char *vend = (char*)memchr(vstart, ' ', lend - vstart);
+        if(vend) {err = EPROTO; goto err2;}
+        size_t vsz = lend - vstart;
+        if(nsz == 7 && memcmp(nstart, "Upgrade", 7) == 0) {
+            if(hasupgrade || vsz != 9 || memcmp(vstart, "websocket", 9) != 0) {
+                err = EPROTO; goto err2;}
+            hasupgrade = 1;
+            continue;
+        }
+        if(nsz == 10 && memcmp(nstart, "Connection", 10) == 0) {
+            if(hasconnection || vsz != 7 || memcmp(vstart, "Upgrade", 7) != 0) {
+                err = EPROTO; goto err2;}
+            hasconnection = 1;
+            continue;
+        }
+        if(nsz == 20 && memcmp(nstart, "Sec-WebSocket-Accept", 20) == 0) {
+            /* TODO */
+            haskey = 1;
+            continue;
+        }
+    }
+    if(!hasupgrade || !hasconnection || !haskey) {err = EPROTO; goto err2;}
+
+    return s;
+
+err2:
+    tcpclose(s->u);
+err1:
+    free(s);
+err0:
+    errno = err;
+    return NULL;
 }
 
 const char *wsockurl(wsock s) {
@@ -231,6 +304,9 @@ size_t wsockrecv(wsock s, void *msg, size_t len) {
 }
 
 void wsockclose(wsock s) {
-    assert(0);
+    /* TODO: Closing handshake. */
+    assert(s->u);
+    tcpclose(s->u);
+    free(s);
 }
 
